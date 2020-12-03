0d17d9f23d059bbb7b51c1aaf335ea463f482330
Rob Giusti <therobgiusti@gmail.com>
2020-12-02 20:08:58 -0800

    initial, with days 1 and 2

---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2020 {
+    internal class Program {
+        public static void Main(string[] args) {
+            var day1 = new Day1();
+            var day2 = new Day2();
+            
+            //day1.Run();
+            day2.Run();
+        }
+    }
+    
+    public interface Day {
+        void Run();
+    }
+}
