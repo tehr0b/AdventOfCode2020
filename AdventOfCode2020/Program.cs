5daffe43732c1abe2cfbe438e05fdef977e6de8a
Rob Giusti <therobgiusti@gmail.com>
2020-12-04 12:52:29 -0800

    Day 3 & 4

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,11 +1,26 @@
-﻿namespace AdventOfCode2020 {
+﻿using System;
+using System.Diagnostics;
+
+namespace AdventOfCode2020 {
     internal class Program {
         public static void Main(string[] args) {
-            var day1 = new Day1();
-            var day2 = new Day2();
+            
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            
+            //var day1 = new Day1();
+            //var day2 = new Day2();
+            //var day3 = new Day3();
+            var day4 = new Day4();
             
             //day1.Run();
-            day2.Run();
+            //day2.Run();
+            //day3.Run();
+            day4.Run();
+            
+            stopwatch.Stop();
+            
+            Console.WriteLine($"Finished running in {stopwatch.ElapsedMilliseconds} ms");
         }
     }
     
